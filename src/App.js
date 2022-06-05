3c11431b0e0126b409296a3c608f781c5591967f
Barun Debnath <barundebnath91@gmail.com>
2022-06-05 18:23:13 +0530

    Header UI done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Header } from "./components";
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-blue-600">
-      App
+    <div className="w-screen h-auto flex flex-col">
+      <Header />
     </div>
   );
 };
