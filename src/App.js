dd6f80cdb4053a8d78dedb073c7896f4a1a8c1dd
Barun Debnath <barundebnath91@gmail.com>
2022-06-06 01:06:33 +0530

    logout | new item admin option | menu UI done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Header, CreateContainer, MainContainer } from "./components";
 
 const App = () => {
   return (
-    <AnimatePresence>
+    <AnimatePresence exitBeforeEnter>
       <div className="w-screen h-auto flex flex-col bg-primary">
         <Header />
         <main className="mt-24 p-8 w-full">
