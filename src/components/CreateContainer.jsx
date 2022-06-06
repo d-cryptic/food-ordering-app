36fb28dd4d7916ddd806000e65e6901a3a85fc52
Barun Debnath <barundebnath91@gmail.com>
2022-06-06 16:48:37 +0530

    Create item form | Select Category | Upload image

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
-import { MdFastfood } from "react-icons/md";
+import { MdCloudUpload, MdDelete, MdFastfood } from "react-icons/md";
+import { categories } from "../utils/data";
+import { Loader } from "../components";
 
 const CreateContainer = () => {
   const [title, setTitle] = useState("");
@@ -14,9 +16,12 @@ const CreateContainer = () => {
   const [msg, setMsg] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  const uploadImage = () => {};
+  const deleteImage = () => {};
+
   return (
     <div className="w-full min-h-screen h-auto flex items-center justify-center">
-      <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center">
+      <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
         {fields && (
           <motion.p
             initial={{ opacity: 0 }}
@@ -41,6 +46,71 @@ const CreateContainer = () => {
             placeholder="Give me a title..."
             className="w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-400 text-textColor"
           />
+        </div>
+
+        <div className="w-full ">
+          <select
+            onChange={(e) => setCategory(e.target.value)}
+            name=""
+            id=""
+            className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer">
+            <option value="other" className="bg-white">
+              Select Category
+            </option>
+            {categories &&
+              categories.map((item) => (
+                <option
+                  value={item.urlParamName}
+                  key={item.id}
+                  className="text-base border-0 outline-none capitalize bg-white text-headingColor">
+                  {item.name}
+                </option>
+              ))}
+          </select>
+        </div>
+
+        <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg">
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              {!imageAsset ? (
+                <>
+                  <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+                    <div className="w-full h-full flex flex-col items-center justify-center gap-2">
+                      <MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700" />
+                      <p className="text-gray-500 hover:text-gray-700">
+                        Click here to upload
+                      </p>
+                    </div>
+                    <input
+                      type="file"
+                      name="uploadimage"
+                      accept="image/*"
+                      onChange={uploadImage}
+                      className="w-0 h-0"
+                    />
+                  </label>
+                </>
+              ) : (
+                <>
+                  <div className="relative h-full">
+                    <img
+                      src={imageAsset}
+                      alt="uploaded image"
+                      className="w-full h-full object-cover"
+                    />
+                    <button
+                      type="button"
+                      className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out"
+                      onClick={deleteImage}>
+                      <MdDelete className="text-white" />
+                    </button>
+                  </div>
+                </>
+              )}
+            </>
+          )}
         </div>
       </div>
     </div>
