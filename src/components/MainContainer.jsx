a8c7941915f505437a868a6a9c91feda30e2c1f1
Barun Debnath <barundebnath91@gmail.com>
2022-06-07 10:17:31 +0530

    slider arrow | home done

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import HomeContainer from "./HomeContainer";
+import { motion } from "framer-motion";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const MainContainer = () => {
   return (
@@ -12,12 +14,16 @@ const MainContainer = () => {
           </p>
 
           <div className="hidden md:flex gap-3 items-center">
-            <div className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500  cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center ">
-              {" "}
-            </div>
-            <div className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center ">
-              {" "}
-            </div>
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500  cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center ">
+              <MdChevronLeft className="text-base text-white" />
+            </motion.div>
+            <motion.div
+              whileTap={{ scale: 0.75 }}
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center ">
+              <MdChevronRight className="text-lg text-white" />
+            </motion.div>
           </div>
         </div>
       </section>
