8e34f122bc040c052aa39ec73dd64a7b272d74cd
Barun Debnath <barundebnath91@gmail.com>
2022-06-08 00:39:03 +0530

    updated

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -5,10 +5,8 @@ import { heroData } from "../utils/data";
 
 const HomeContainer = () => {
   return (
-    <section
-      className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full "
-      id="home">
-      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
+      <div className="py-2  flex-1 flex flex-col items-start justify-center  gap-6">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
           <p className="text-base text-orange-500 font-semibold">
             Bike Delivery
@@ -17,59 +15,61 @@ const HomeContainer = () => {
             <img
               src={Delivery}
               className="w-full h-full object-contain"
-              alt="delivery"
+              alt="Delivery icon"
             />
           </div>
         </div>
 
         <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
-          The Fastest Delivery in
-          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
+          The Fastest Delivery in{" "}
+          <span className="text-orange-600 text-[3rem] md:text-[5rem]">
             Your City
           </span>
         </p>
 
         <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima velit
-          eaque fugit distinctio est nam voluptatum architecto, porro iusto
-          deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
-          suscipit!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+          blanditiis vel nostrum aliquam error minima molestias quasi, sapiente
+          voluptatum. Harum, quis aliquid! A quaerat perspiciatis id sit
+          incidunt accusantium enim?
         </p>
 
         <button
           type="button"
-          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100">
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100">
           Order Now
         </button>
       </div>
-      <div className="py-2 flex-1 flex items-center relative">
+
+      <div className="py-2  flex-1 flex items-center relative">
         <img
           src={HeroBg}
-          className=" ml-auto h-420 w-full lg:w-auto lg:h-650"
+          className="ml-auto h-420 w-full lg:w-auto lg:h-650 "
           alt="hero-bg"
         />
 
-        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
+        <div className="w-full h-full top-0 left-0 absolute flex items-center justify-center  py-4 gap-4 flex-wrap lg:px-16 ">
           {heroData &&
             heroData.map((n) => (
               <div
                 key={n.id}
-                className="  lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg">
+                className=" lg:w-190 p-4  bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg ">
                 <img
                   src={n.imageSrc}
-                  className="w-20 lg:w-40 -mt-10 lg:-mt-20 "
-                  alt="I1"
+                  alt="icecream"
+                  className=" w-20 lg:w-40 -mt-10 lg:-mt-20"
                 />
-                <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+                <p className=" text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
                   {n.name}
                 </p>
 
-                <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
-                  {n.decp}
+                <p className="text-[12px] lg:text-sm  text-lighttextGray my-1 lg:my-3">
+                  {n.description}
                 </p>
 
                 <p className="text-sm font-semibold text-headingColor">
-                  <span className="text-xs text-red-600">$</span> {n.price}
+                  <span className="text-xs text-red-600">$</span>
+                  {n.price}
                 </p>
               </div>
             ))}
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,39 +1,37 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import HomeContainer from "./HomeContainer";
 import { motion } from "framer-motion";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
-import RowContainer from "./RowContainer";
+import { RowContainer, CartContainer } from "./";
 import { useStateValue } from "../context/StateProvider";
-import MenuContainer from "./MenuContainer";
-import CartContainer from "./CartContainer";
+import { MenuContainer } from "./";
 
 const MainContainer = () => {
   const [{ foodItems, cartShow }, dispatch] = useStateValue();
   const [scrollValue, setScrollValue] = useState(0);
 
-  useEffect(() => {}, [scrollValue, cartShow]);
+  useEffect(() => {}, [scrollValue]);
 
   return (
-    <div className="w-full h-auto flex flex-col items-center justify-center ">
+    <div className="w-full h-auto flex flex-col items-center justify-center">
       <HomeContainer />
-
       <section className="w-full my-6">
         <div className="w-full flex items-center justify-between">
-          <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
+          <p className="text-2xl font-semibold capitalize text-headingColor  relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
             Our fresh & healthy fruits
           </p>
 
           <div className="hidden md:flex gap-3 items-center">
             <motion.div
               whileTap={{ scale: 0.75 }}
-              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer  hover:shadow-lg flex items-center justify-center"
-              onClick={() => setScrollValue(-200)}>
-              <MdChevronLeft className="text-lg text-white" />
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500  cursor-pointer hover:shadow-lg flex items-center justify-center "
+              onClick={() => setScrollValue(scrollValue - 300)}>
+              <MdChevronLeft className="text-base text-white" />
             </motion.div>
             <motion.div
               whileTap={{ scale: 0.75 }}
-              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
-              onClick={() => setScrollValue(200)}>
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer hover:shadow-lg flex items-center justify-center"
+              onClick={() => setScrollValue(scrollValue + 300)}>
               <MdChevronRight className="text-lg text-white" />
             </motion.div>
           </div>
@@ -46,7 +44,6 @@ const MainContainer = () => {
       </section>
 
       <MenuContainer />
-
       {cartShow && <CartContainer />}
     </div>
   );
