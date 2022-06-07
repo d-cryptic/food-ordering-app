c4fdd8118db8f934510393898e148f0a3e33475c
Barun Debnath <barundebnath91@gmail.com>
2022-06-07 15:19:27 +0530

    Slider done

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import HomeContainer from "./HomeContainer";
 import { motion } from "framer-motion";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
@@ -7,6 +7,9 @@ import { useStateValue } from "../context/StateProvider";
 
 const MainContainer = () => {
   const [{ foodItems }, dispatch] = useStateValue();
+  const [scrollValue, setScrollValue] = useState(0);
+
+  useEffect(() => {}, [scrollValue]);
 
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
@@ -20,21 +23,25 @@ const MainContainer = () => {
           <div className="hidden md:flex gap-3 items-center">
             <motion.div
               whileTap={{ scale: 0.75 }}
-              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500  cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center ">
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500  cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center "
+              onClick={() => setScrollValue(scrollValue - 300)}>
               <MdChevronLeft className="text-base text-white" />
             </motion.div>
             <motion.div
               whileTap={{ scale: 0.75 }}
-              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center ">
+              className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
+              onClick={() => setScrollValue(scrollValue + 300)}>
               <MdChevronRight className="text-lg text-white" />
             </motion.div>
           </div>
         </div>
         <RowContainer
+          scrollValue={scrollValue}
           flag={true}
           data={foodItems?.filter((n) => n.category === "fruits")}
         />
       </section>
+      <section className="w-full my-6"></section>
     </div>
   );
 };
--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { MdShoppingBasket } from "react-icons/md";
 import { motion } from "framer-motion";
 
-const RowContainer = ({ flag, data }) => {
+const RowContainer = ({ flag, data, scrollValue }) => {
+  const rowContainer = useRef();
+  useEffect(() => {
+    rowContainer.current.scrollLeft = scrollValue;
+  }, [scrollValue]);
+
   return (
     <div
-      className={`w-full my-12  ${
-        flag ? "overflow-x-scroll" : "overflow-x-hidden"
+      ref={rowContainer}
+      className={`w-full h-full flex items-center my-12 gap-3 scroll-smooth  ${
+        flag
+          ? "overflow-x-scroll scrollbar-none"
+          : "overflow-x-hidden flex-wrap"
       }`}>
       {data &&
         data.map((item) => (
           <div
             key={item.id}
-            className=" w-300 md:w-340 h-auto my-12   
-		   backdrop-blur-lg bg-cardOverlay rounded-lg p-2 hover:drop-shadow-lg">
-            <div className="w-full flex items-center justify-between">
+            className="w-225 h-[200px] min-w-[275px] md:w-300 md:min-w-[300px]  my-12   
+		   backdrop-blur-lg bg-cardOverlay rounded-lg py-2 px-4 hover:drop-shadow-lg flex flex-col items-center justify-evenly">
+            <div className="w-full h-full flex items-center justify-between">
               <motion.img
                 whileHover={{ scale: 1.2 }}
-                src="https://firebasestorage.googleapis.com/v0/b/restaurantapp-2e605.appspot.com/o/Images%2F1654537602515-i5.png?alt=media&token=d9679f2a-b456-4d87-bbab-659449107981"
+                src={item?.imageURL}
                 alt=""
-                className="w-40 -mt-8 drop-shadow-2xl"
+                className="h-[125px] w-[145px] -mt-16 drop-shadow-2xl"
               />
               <motion.div
                 whileTap={{ scale: 0.75 }}
@@ -30,12 +38,15 @@ const RowContainer = ({ flag, data }) => {
 
             <div className="w-full flex flex-col gap-4 items-end justify-end">
               <p className="text-textColor font-semibold text-base md:text-lg">
-                Chocolate & Vanilla
+                {item?.title}
               </p>
-              <p className="mt-1 text-sm text-gray-500">45 Calories</p>
+              <p className="mt-1 text-sm text-gray-500">
+                {item?.calories} Calories
+              </p>
               <div className="flex items-center gap-8">
                 <p className="text-lg text-headingColor font-semibold">
-                  <span className="text-sm text-red-500">$</span>5.25{" "}
+                  <span className="text-sm text-red-500">$</span>
+                  {item?.price}{" "}
                 </p>
               </div>
             </div>
